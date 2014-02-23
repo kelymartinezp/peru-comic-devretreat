4851db87d2b4e65e6bfbf9af1c4fc070ec6b43e1
Ronald Velasquez <ronaldvg93@gmail.com>
2014-02-23 09:45:24 -0500

    Import inicial de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeruComicDevretreat::Application.config.secret_key_base = '929f4afbede98df3786dbe1fb140f98748625ce7ee14f18a95cd01328c5c9842ec00d43f244c60daa56a9b73d615996720fd26c9667a3e0c2180ad36b1c0b21f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PeruComicDevretreat::Application.config.session_store :cookie_store, key: '_peru-comic-devretreat_session'
